a481af78531dca73d013be5b9df47d465972b019
Manik Dahiya <81329364+manik28mar@users.noreply.github.com>
2024-12-26 17:11:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// var elem = document.querySelector('.main-carousel');
+// var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true,
+//   wrapAround: true,
+//   autoPlay: 2000
+// });
+
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
