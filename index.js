e19a24a5bfa79320562e952d63e3bbf487dedbfe
Manik Dahiya <81329364+manik28mar@users.noreply.github.com>
2024-12-30 17:46:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -26,3 +26,15 @@ $('.main-carousel').flickity({
   function closeNav() {
     document.getElementById("nav-bar").style.width = "0%";
   }
+
+const myInterval123999 = setInterval(function(){
+    if(document.querySelector('.actions input') !== null){
+        document.querySelector('[data-application-name="embedded-viral-link-ui"]').style.display = 'none';
+        document.querySelector('.actions input').style.backgroundColor = '#3d0d6d';
+        document.querySelector('.actions input').style.border = '1px solid #FFFFFF';
+        clearInterval(myInterval123999);
+    }
+},100);
+
+
+
